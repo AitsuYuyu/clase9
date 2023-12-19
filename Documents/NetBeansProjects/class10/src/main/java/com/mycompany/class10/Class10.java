556ef5c609fbae9644a502ca8501f193476af54d
user <user@RootCampus.lan>
2023-12-19 16:55:06 -0500

    clase 9

---
+++ Documents/NetBeansProjects/class10/src/main/java/com/mycompany/class10/Class10.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.class10;
+
+/**
+ *
+ * @author user
+ */
+public class Class10 {
+
+    public static void main(String[] args) {
+    }
+}
---
+++ Documents/NetBeansProjects/class10/src/main/java/com/mycompany/class10/Empresa.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.class10;
+
+/**
+ *
+ * @author user
+ */
+public class Empresa {
+
+    
+}
